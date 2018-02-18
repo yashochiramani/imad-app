ac7e9fc1d73b4e706b3672aba4d7382cf8b9339a
yashochiramani <me160003036@iiti.ac.in>
2018-02-19 00:27:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/index', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
